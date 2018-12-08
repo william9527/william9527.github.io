b13bfa4f2ad08ba90fa8b68bd095b8f8e4b43e06
william9527 <william@rayaegis.com>
2018-12-08 17:41:07 +0800

    Update install_victim.sh

--- scripts/install_victim.sh
+++ scripts/install_victim.sh
@@ -1,2 +1,2 @@
-docker run --rm -it -p 80:80 nginx
-docker run --rm -it -p 8080:80 vulnerables/web-dvwa
+docker run --rm -it -d -p 80:80 nginx
+docker run --rm -it -d -p 8080:80 vulnerables/web-dvwa
